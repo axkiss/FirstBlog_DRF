f234eab939d241eafe7bf03feb0bdb42ea0e2c6a
axkiss <axkiss@yandex.ru>
2022-06-24 17:20:07 +0700

    fix: use environment variable

--- Backend/blog_project_drf/settings.py
+++ Backend/blog_project_drf/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g*e^y-3cmty=8a!^+7k612&9g2^gl57ivvtzlp6tiq4n&9_0^9'
-# SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
@@ -108,7 +107,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'firstblog_drf',
         'USER': 'firstuser_drf',
-        'PASSWORD': 'matvey',
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
         'HOST': 'localhost',
         'PORT': '',
     }
