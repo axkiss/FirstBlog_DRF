c1df309ac7220aa8c24f7b598e92a8ab6fa0a029
axkiss <axkiss@yandex.ru>
2022-06-26 15:02:39 +0700

    Add: View for list of posts and detail

--- Backend/blog_api/views.py
+++ Backend/blog_api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
 
-# Create your views here.
+from .serializers import PostSerializer
+from .models import Post
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    """List of posts and detail"""
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+    lookup_field = 'slug'
+    permission_classes = [permissions.AllowAny]
