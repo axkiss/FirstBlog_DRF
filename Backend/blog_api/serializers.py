a5e9eb3ac96c5c79d5b5fab947fd17cfa5a571cc
axkiss <axkiss@yandex.ru>
2022-06-26 17:56:09 +0700

    Add: Serializer for tags

--- Backend/blog_api/serializers.py
+++ Backend/blog_api/serializers.py
@@ -1,13 +1,22 @@
 from rest_framework import serializers
 
+from taggit.models import Tag
+from taggit_serializer.serializers import TagListSerializerField, TaggitSerializer
+
 from users_api.models import User
 from .models import Post
-from taggit_serializer.serializers import TagListSerializerField, TaggitSerializer
+
+
+class TagSerializer(serializers.ModelSerializer):
+    """Serializer for tags"""
+    class Meta:
+        model = Tag
+        fields = ('name', 'slug')
 
 
 class PostSerializer(TaggitSerializer, serializers.ModelSerializer):
     """Serializer for model Post"""
-    tag = TagListSerializerField()
+    tag = TagSerializer(many=True, read_only=True)
     author = serializers.SlugRelatedField(slug_field="username", queryset=User.objects.all())
 
     class Meta:
