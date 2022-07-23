c31a42762c6e5681cb31f69943ff88d4333aa5c6
axkiss <axkiss@yandex.ru>
2022-07-23 21:35:08 +0700

    Add: url for register and email confirm

--- Backend/users_api/urls.py
+++ Backend/users_api/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+from . import views
+
+app_name = 'users_api'
+
 urlpatterns = [
-    path("token/", TokenObtainPairView.as_view(), name="token"),
-    path("refresh_token/", TokenRefreshView.as_view(), name="refresh_token"),
+    path('token/', TokenObtainPairView.as_view(), name='token'),
+    path('refresh_token/', TokenRefreshView.as_view(), name='refresh_token'),
+    path('register/', views.RegisterView.as_view(), name='register'),
+    path('email_confirm/<uidb64>/<token>/', views.EmailConfirmVerifyView.as_view(), name='email_confirm_verify'),
 ]
