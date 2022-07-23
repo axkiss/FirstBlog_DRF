fdd94157e9737bc3498ad63184e1fafa6311ce84
axkiss <axkiss@yandex.ru>
2022-07-23 21:34:38 +0700

    Add: func 'send email to verify'

--- Backend/users_api/utils.py
+++ Backend/users_api/utils.py
@@ -1,5 +1,46 @@
 from PIL import Image
 
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+from django.contrib.auth.tokens import default_token_generator as token_generator
+
+
+def send_email_for_verify(request, user):
+    """
+    Sending email with link for verify user email address
+    :param request:
+    :param user:
+    :return:
+    """
+    email_template_name = 'users_api/email_confirm_email.html',
+    # SeoData = apps.get_model('blog_app', 'SeoData')
+    # seo_data = SeoData.objects.first()
+    # if seo_data:
+    #     site_name = seo_data.site_name
+    #     domain = seo_data.domain
+    # else:
+    current_site = get_current_site(request)
+    site_name = current_site.name
+    domain = current_site.domain
+    context = {
+        'domain': domain,
+        'site_name': site_name,
+        'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+        'user': user,
+        'token': token_generator.make_token(user),
+        'protocol': request.scheme,
+    }
+
+    message = render_to_string(email_template_name, context=context)
+    email = EmailMessage(f'Verify email - {site_name}',
+                         message,
+                         to=[user.email],
+                         )
+    email.send()
+
 
 def crop_img_to_square(image):
     """
