d76fbf06a14910239ea05967127d6b62f4ff2012
axkiss <axkiss@yandex.ru>
2022-07-23 21:37:08 +0700

    Add: display users in admin panel

--- Backend/users_api/admin.py
+++ Backend/users_api/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+User = get_user_model()
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'full_name', 'group', 'is_staff')
+    list_per_page = 25
+
+    @admin.display(description='FULL NAME')
+    def full_name(self, obj):
+        return obj.get_full_name()
+
+    @admin.display(description='GROUP')
+    def group(self, obj):
+        return obj.get_group()
