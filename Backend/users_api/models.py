9cb3b66b5b132a2aabdc3321ed951e16e381daca
axkiss <axkiss@yandex.ru>
2022-07-23 21:36:31 +0700

    Add: param 'unique' for email

--- Backend/users_api/models.py
+++ Backend/users_api/models.py
@@ -7,7 +7,13 @@ class User(AbstractUser):
     """Users on blog"""
     first_name = models.CharField(_('first name'), max_length=150)
     last_name = models.CharField(_('last name'), max_length=150)
-    email = models.EmailField(_('email address'))
+    email = models.EmailField(
+        _('email address'),
+        unique=True,
+        error_messages={
+            "unique": _("A user with that email already exists."),
+        },
+    )
     email_verify = models.BooleanField(default=False)
 
     # def has_perm_add_post(self):
@@ -16,8 +22,8 @@ class User(AbstractUser):
     # def has_perm_edit_post(self):
     #     return self.has_perm('blog_app.change_post')
     #
-    # def get_group(self):
-    #     return self.groups.all().first()
+    def get_group(self):
+        return self.groups.all().first()
     #
     # def get_absolute_url(self):
     #     return reverse('users:profile', args=[self.username])
