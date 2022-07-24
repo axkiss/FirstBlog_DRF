f1a129cc8b5d606db4e05ad2c77dc487057a8622
axkiss <axkiss@yandex.ru>
2022-07-24 15:21:07 +0700

    Add: Url for user profile

--- Backend/users_api/urls.py
+++ Backend/users_api/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('refresh_token/', TokenRefreshView.as_view(), name='refresh_token'),
     path('register/', views.RegisterView.as_view(), name='register'),
     path('email_confirm/<uidb64>/<token>/', views.EmailConfirmVerifyView.as_view(), name='email_confirm_verify'),
+    path('profile/<str:username>/', views.UserProfileView.as_view(), name='profile')
 ]
