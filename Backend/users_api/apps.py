a6c966042933161c7821f01fceee8f5d216c83d6
axkiss <axkiss@yandex.ru>
2022-08-08 14:55:16 +0700

    Add: signals

--- Backend/users_api/apps.py
+++ Backend/users_api/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class UsersApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users_api'
+
+    def ready(self):
+        import users_api.signals
